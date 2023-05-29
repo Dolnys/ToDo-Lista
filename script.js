1826769200d28f81f3f7811ed05df71092757341
Dolnys <thedolnys@gmail.com>
2023-05-29 16:14:32 +0200

    add new todo function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let todoInput;
 let errorInfo;
 let addBtn;
 let ulList;
+let newTodo;
 
 const main = () => {
   prepareDOMElements();
@@ -15,6 +16,20 @@ const prepareDOMElements = () => {
   ulList = document.querySelector('.todoList ul');
 };
 
-const prepareDOMEvents = () => {};
+const prepareDOMEvents = () => {
+  addBtn.addEventListener('click', addNewTodo);
+};
+
+const addNewTodo = (params) => {
+  if (todoInput.value !== '') {
+    newTodo = document.createElement('li');
+    newTodo.textContent = todoInput.value;
+    ulList.append(newTodo);
+    todoInput.value = '';
+    errorInfo.textContent = '';
+  } else {
+    errorInfo.textContent = 'Please enter the task content';
+  }
+};
 
 document.addEventListener('DOMContentLoaded', main);
